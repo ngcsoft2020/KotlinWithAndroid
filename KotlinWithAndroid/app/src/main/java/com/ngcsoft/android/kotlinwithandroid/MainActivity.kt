159b5527f6d2251390778994c64fb96c2172eaa4
Ganesh C <ganeshcht8@gmail.com>
2020-09-05 15:23:44 +0530

    Handle button click listener

--- KotlinWithAndroid/app/src/main/java/com/ngcsoft/android/kotlinwithandroid/MainActivity.kt
+++ KotlinWithAndroid/app/src/main/java/com/ngcsoft/android/kotlinwithandroid/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.ngcsoft.android.kotlinwithandroid
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity :AppCompatActivity (){
@@ -9,5 +11,11 @@ class MainActivity :AppCompatActivity (){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Remove extra boiler code to declare and initialize btn_click variable
+        //Here we use high-level lamda-expression functionality for onClick operation
+        btn_click.setOnClickListener {
+            //write code here
+            Toast.makeText(this,"I am Clicked!",Toast.LENGTH_SHORT).show()
+        }
     }
 }
